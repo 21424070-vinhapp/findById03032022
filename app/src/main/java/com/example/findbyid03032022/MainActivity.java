b2ed5e960a8534f119fef03f5ce754a436ae4a8b
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-03-03 16:20:05 +0700

    tao event cho button thong qua findbyid

--- app/src/main/java/com/example/findbyid03032022/MainActivity.java
+++ app/src/main/java/com/example/findbyid03032022/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.findbyid03032022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button mBtnOk=findViewById(R.id.btnOk);
+    TextView mTxtHello=findViewById(R.id.txtHello);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTxtHello.setText("Hello FindById");
+            }
+        });
     }
 }
